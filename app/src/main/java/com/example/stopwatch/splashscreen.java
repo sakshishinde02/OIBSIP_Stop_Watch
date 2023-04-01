2065dc33565f84fc942fa43438cc40fc370c2ab5
unknown <sakshishinde2386@gmail.com>
2023-04-01 20:15:44 +0530

    first commit

---
+++ app/src/main/java/com/example/stopwatch/splashscreen.java
@@ -0,0 +1,24 @@
+package com.example.stopwatch;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashscreen  extends AppCompatActivity  {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent =new Intent(splashscreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },2000);
+    }
+}
